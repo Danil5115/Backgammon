77b28236f569526ff158d79023ac51f6f1025932
Danil5115 <kordp51@gmail.com>
2023-06-04 23:36:12 +0200

    Specifying a class for the game, in which the main conditions are determined, including the possibility of saving the course of the game

--- game.py
+++ game.py
@@ -15,3 +15,46 @@ def prompt_for_input_from_list(options: set): #запрос ответа у по
         if response not in options:
             print("Invalid selection.")
     return response
+
+class BackgammonGame(Backgammon): # запуск
+    START_BOARD = [0, 2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2, 0]
+
+    def __init__(self, start_board=START_BOARD, start_new_game=True):
+        white_to_move = self.determine_start_player()
+        super().__init__(start_board, white_to_move)
+        print("""White moving:      Black moving:
+    ->->|              <-<-^
+    <-<-ˇ              ->->|
+    """)
+        self.dice = []
+        if start_new_game:
+            self.turn()
+
+    def save_game(self): #json
+        state = {
+            "board": self.board,
+            "white_to_move": self.white_to_move,
+            "dice": self.dice,
+            "white captured": self.white_captured,
+            "black captured": self.black_captured,
+            "white bar count": self.white_to_bar,
+            "black bar count": self.black_to_bar,
+            "white_moves": self.white_moves,  # Сохраняем список перемещений белых камней
+            "black_moves": self.black_moves  # Сохраняем список перемещений черных камней
+        }
+
+        with open("game_state.json", "w") as file:
+            json.dump(state, file)
+
+    def load_game(self):
+        if os.path.exists("game_state.json"):
+            with open("game_state.json", "r") as file:
+                state = json.load(file)
+                self.board = state["board"]
+                self.white_to_move = state["white_to_move"]
+                self.dice = state["dice"]
+                self.white_moves = state["white_moves"]  # Загружаем список перемещений белых камней
+                self.black_moves = state["black_moves"]  # Загружаем список перемещений черных камней
+        else:
+            print("No saved game found.")
+        self.turn()
